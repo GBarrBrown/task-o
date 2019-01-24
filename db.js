d6495394e411c586e2b8e104eaaccb79879146b2
JamesCrimp <James.Crimp@gmail.com>
2019-01-24 17:54:14 +1300

    add getTasks and getUsers functions

--- db.js
+++ db.js
@@ -6,3 +6,13 @@ module.exports = {
 }
 
 
+function getTasks(UID, db = connection){
+    return connection('users').select()
+        .join('task_user', 'users.id', 'task_user.user_id')
+        .join('tasks', 'task_user.task_id', 'tasks.id')
+        .where('users.id', UID)
+}
+
+function getUsers(db = connection){
+    return connection('users').select()
+}
