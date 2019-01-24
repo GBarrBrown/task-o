5113b739ac40b65fa0e36a7d58713d08ad91aead
Katene Jack Cotton <katenejack@gmail.com>
2019-01-24 16:15:34 +1300

    removed unnecessary files

--- db.js
+++ db.js
@@ -3,14 +3,6 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  getUser: getUser,
-  getUsers: getUsers
 }
 
-function getUsers (db = connection) {
-  return db('users').select()
-}
 
-function getUser (id, db = connection) {
-  return db('users').where('id', id).first()
-}
