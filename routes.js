900e11164bae09f70fe34a4630abae24e79b1a46
Katene Jack Cotton <katenejack@gmail.com>
2019-01-25 10:01:09 +1300

    route to homepage from login completed

--- routes.js
+++ routes.js
@@ -15,14 +15,24 @@ router.get('/login', (req, res) => {
 router.post('/login', (req,res) => {
     return db.getUser (req.body.email)
     .then((user) => {
-        return db.getAllTasks(user.id)
+        var id = user[0].id
+        res.redirect('/' + id + '/home')
     })
-    .then((tasks) => {
-        res.render('home', tasks)
-    })
+    .catch(err => {
+        console.log(err.message)
+        res.status(500).send('error')
+      })
 })
 
 router.get('/:id/home', (req, res)=> {
-    res.render('home')
+    return db.getAllTasks(req.params.id)
+    .then((tasks) =>{
+        res.render('home',tasks)
+    })
+    .catch(err => {
+        console.log(err.message)
+        res.status(500).send('error')
+      })
 })
+
 module.exports = router
