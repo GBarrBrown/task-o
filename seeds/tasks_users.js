f75921f8dd12c6afabccea871a99ea6b0b197bb1
JamesCrimp <James.Crimp@gmail.com>
2019-01-25 15:00:52 +1300

    Add seed to task_users table

--- seeds/tasks_users.js
+++ seeds/tasks_users.js
@@ -8,12 +8,30 @@ exports.seed = function(knex, Promise) {
         {user_id:1, task_id:1},
         {user_id:1, task_id:2},
         {user_id:1, task_id:3},
+        {user_id:1, task_id:4},
+        {user_id:1, task_id:5},
+        {user_id:1, task_id:6},
+        {user_id:1, task_id:7},
+        {user_id:1, task_id:8},
+        {user_id:1, task_id:9},
         {user_id:2, task_id:1},
         {user_id:2, task_id:2},
         {user_id:2, task_id:3},
+        {user_id:2, task_id:4},
+        {user_id:2, task_id:5},
+        {user_id:2, task_id:6},
+        {user_id:2, task_id:7},
+        {user_id:2, task_id:8},
+        {user_id:2, task_id:9},
         {user_id:3, task_id:1},
         {user_id:3, task_id:2},
-        {user_id:3, task_id:3}
+        {user_id:3, task_id:3},
+        {user_id:3, task_id:4},
+        {user_id:3, task_id:5},
+        {user_id:3, task_id:6},
+        {user_id:3, task_id:7},
+        {user_id:3, task_id:8},
+        {user_id:3, task_id:9}
       ]);
     });
 };
